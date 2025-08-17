5ddcffce9b487804758a310616d8b035f3849166
magnetrwn <rpolli.dev@tutanota.com>
2025-08-17 13:34:17 +0200

    Initial commit, setup GN build system and shell script.

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+set -e
+
+if ! command -v gn &> /dev/null || ! command -v ninja &> /dev/null; then
+  echo "GN and Ninja must be installed to build this project."
+  exit 1
+fi
+
+BUILD_TYPE=${1:-Debug}
+if [[ "$BUILD_TYPE" != "Debug" && "$BUILD_TYPE" != "Release" ]]; then
+  echo "Usage: $0 <Debug [default] | Release>"
+  exit 1
+fi
+
+gn gen out/"$BUILD_TYPE" --args="is_debug=$([ "$BUILD_TYPE" == "Debug" ] && echo true || echo false)"
+ninja -C out/"$BUILD_TYPE" demo
+./out/"$BUILD_TYPE"/obj/demo || { echo "Build failed."; exit 1; }
